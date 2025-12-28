ec998c8aaa1fa6534e0869681e263c800a099e42
Omar <omarei@omoto.ai>
2025-12-28 14:55:02 +0000

    Ignore TypeScript errors in build

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -15,6 +15,9 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
